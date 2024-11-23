0e3acfb234f27394b5aa7e7880ca3d18a2a6d2c6
Shohel Rana <bds5r3mizi@gmail.com>
2024-11-23 22:28:46 +0600

    appent info

--- app.js
+++ app.js
@@ -10,6 +10,45 @@
 
 // const seatLeft = document.getElementById("seats-left");
 // const convertedSeatLeft = parseInt(seatLeft);
+const allSeat = document.getElementsByClassName("seat");
+
+for (const seat of allSeat) {
+    seat.addEventListener("click", function(event) {
+        const a1 = event.target.parentNode.childNodes[5].innerText; 
+
+        const a2 = event.target.parentNode.childNodes[7].innerText;
+        
+        const a3 = event.target.parentNode.childNodes[9].innerText;
+
+        const a4 = event.target.parentNode.childNodes[11].innerText;
+
+        const selectedContainer = document.getElementById("selected-append");
+// creating div
+        const div = document.createElement("div");
+
+        // creating p
+
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+
+        p1.innerText = name;
+        p2.innerText = price;
+        p3.innerText = category;
+// append info
+        div.append(p1)
+        div.append(p2)
+        div.append(p3)
+
+        // console.log(a1,a2,a3,a4);
+    })
+}
+
+// console.log(allSeat);
+
+
+
+
 const seatLeft = getConvertedValue("seats-left"); 
 
 const seatCounter = getConvertedValue("seat-counter");
